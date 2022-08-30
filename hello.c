402c09b480ef04318138c50b008f9a4420fb2467
Shoaib Mairaj <shoaibmairaj@shoaib-mairaj>
2022-08-30 14:09:45 +0530

    Revert "Merge pull request #1 from shoaibmairaj/newbranch" This reverts commit 14621f770ca560a66709e4a5f1f5454c0750de3b, reversing changes made to 279e58eb29118862834b659550606c344f047ee1.

--- hello.c
+++ hello.c
@@ -7,4 +7,8 @@ printf(“Hello! I am C-Program. Thank you!”);
 return 0;
 
 }
+<<<<<<< HEAD
 #changed on 17/09/2022
+=======
+
+>>>>>>> parent of 14621f7... Merge pull request #1 from shoaibmairaj/newbranch
