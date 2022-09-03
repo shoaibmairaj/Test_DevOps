70dc1643cbe66eed7b6f5ae65f1353cedd1353e0
Shoaib Mairaj <shoaibmairaj@shoaib-mairaj>
2022-09-03 11:24:48 +0530

    k8s-setup-with-preconfiguration

---
+++ k8s_setup.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Install docker
+sudo apt-get update && sudo apt-get install -y docker.io
+sudo systemctl start docker
+
+#Letting IPTables see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+# Apply changes without rebooting the system
+sudo sysctl --system
+
+# Container runtimes - containerd as CRI runtime
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Apply changes without rebooting the system
+sudo sysctl --system
+
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+sudo apt-get update
+
+# Install imp certificates
+sudo apt install curl apt-transport-https vim git wget gnupg2 \
+software-properties-common apt-transport-https ca-certificates uidmap -y
+
+# Download Google Public Key
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+# Add K8s Repo
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+apt-install containerd -y
+# Install K8s components
+sudo apt-get install -y kubelet=1.22.1-00 kubeadm=1.22.1-00 kubectl=1.22.1-00
+
+# Hold K8s components
+sudo apt-mark hold kubelet kubeadm kubectl
+user1@cpnode:~$ less k8s_ubuntu_setup.sh 
+
+
+sudo apt-get update
+
+# Install imp certificates
+sudo apt install curl apt-transport-https vim git wget gnupg2 \
+software-properties-common apt-transport-https ca-certificates uidmap -y
+
+# Download Google Public Key
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+# Add K8s Repo
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+apt-install containerd -y
+# Install K8s components
+sudo apt-get install -y kubelet=1.22.1-00 kubeadm=1.22.1-00 kubectl=1.22.1-00
+
+# Hold K8s components
+sudo apt-mark hold kubelet kubeadm kubectl
